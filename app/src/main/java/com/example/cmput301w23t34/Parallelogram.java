b5b9ac41006449bdb5259249566ff57555a231ec
Kai Luedemann <kluedema@ualberta.ca>
2023-01-31 19:02:27 -0700

    Added Parallelogram

---
+++ app/src/main/java/com/example/cmput301w23t34/Parallelogram.java
@@ -0,0 +1,16 @@
+package com.example.cmput301w23t34;
+
+public class Parallelogram extends Shape {
+
+    private int l;
+    private int w;
+    private int angle;
+
+    public Parallelogram(int x, int y, int l, int w, int angle) {
+        this.x = x;
+        this.y = y;
+        this.l = l;
+        this.w = w;
+        this.angle = angle;
+    }
+}
