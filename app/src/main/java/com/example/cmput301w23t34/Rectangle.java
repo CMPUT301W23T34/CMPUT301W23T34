72e3a3b0f11395bdaaaba3cb1febded7d9e78614
Yasmin <112932817+yasmindana@users.noreply.github.com>
2023-02-02 10:30:52 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/cmput301w23t34/Rectangle.java
@@ -0,0 +1,27 @@
+package com.example.cmput301w23t34;
+
+public class Rectangle extends Shape {
+    private int base;
+    private int height;
+
+    public Rectangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
