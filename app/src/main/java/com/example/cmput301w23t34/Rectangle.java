0176ac332e1b96d2eb1884fd6729d8407c73d6f5
Yasmin <112932817+yasmindana@users.noreply.github.com>
2023-02-02 10:33:08 -0700

    Update Rectangle.java

--- app/src/main/java/com/example/cmput301w23t34/Rectangle.java
+++ app/src/main/java/com/example/cmput301w23t34/Rectangle.java
@@ -1,27 +1,28 @@
 package com.example.cmput301w23t34;
 
 public class Rectangle extends Shape {
-    private int base;
+    private int width;
     private int height;
 
-    public Rectangle(int base, int height) {
-        this.base = base;
+    public Rectangle(int width, int height) {
+        this.width = width;
         this.height = height;
     }
 
-    public int getBase() {
-        return base;
+    public int getWidth() {
+        return width;
     }
 
     public int getHeight() {
         return height;
     }
 
-    public void setBase(int base) {
-        this.base = base;
+    public void setWidth(int width) {
+        this.width = width;
     }
 
     public void setHeight(int height) {
         this.height = height;
     }
 }
+
