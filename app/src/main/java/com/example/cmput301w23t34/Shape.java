45995b1aea8f88c4549d321420106661760f865c
Brett Merkosky <bamerkos@ualberta.ca>
2023-02-03 12:08:14 -0700

    I had forgotten to save before the last commit

--- app/src/main/java/com/example/cmput301w23t34/Shape.java
+++ app/src/main/java/com/example/cmput301w23t34/Shape.java
@@ -3,9 +3,5 @@ package com.example.cmput301w23t34;
 abstract class Shape{
     int x;
     int y;
-<<<<<<< HEAD
     String color = "Yellow";
-=======
-    String color = "Yellow";
->>>>>>> dd0ffea592165387b0ccd1abde758348147761bf
 }
