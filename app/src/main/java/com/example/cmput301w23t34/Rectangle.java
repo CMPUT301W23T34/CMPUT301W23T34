f8c61b9a32974ba09095c9935dedc2afe99243e5
Yasmin <112932817+yasmindana@users.noreply.github.com>
2023-02-03 15:38:11 -0700

    Resolved Conflict

--- app/src/main/java/com/example/cmput301w23t34/Rectangle.java
+++ app/src/main/java/com/example/cmput301w23t34/Rectangle.java
@@ -3,7 +3,6 @@ package com.example.cmput301w23t34;
 public class Rectangle extends Shape {
     private int width;
     private int height;
-    private String color = "purple";
 
     public Rectangle(int width, int height) {
         this.width = width;
@@ -18,20 +17,12 @@ public class Rectangle extends Shape {
         return height;
     }
 
-    public String getColor() {
-        return color;
-    }
-
     public void setWidth(int width) {
         this.width = width;
     }
 
     public void setHeight(int height) {
         this.height = height;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 }
 
